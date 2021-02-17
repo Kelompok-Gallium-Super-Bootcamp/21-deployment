f15e91d6cd331b27545de21674c64adddbfc6479
Muhammad Choirudin Malik <77602202+choirmalik@users.noreply.github.com>
2021-02-17 17:42:01 +0700

    test

--- tugas/webpack.config.js
+++ tugas/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     performance: './webapp/src/performance/vdom.js',
   },
   output: {
-    path: path.resolve(__dirname, 'www'),
+    path: path.resolve(__dirname, 'webapp/www'),
     filename: '[name].js',
   },
   devtool: 'inline-source-map',
